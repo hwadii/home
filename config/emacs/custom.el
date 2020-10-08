3074a62b8571c1767824c6055f611cc149331143
hwadii <wadii.hajji@etu.univ-amu.fr>
2020-10-09 01:55:29 +0200

    Add prelude config

---
+++ config/emacs/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(yaml-mode web-mode tide yari inf-ruby company-anaconda anaconda-mode lsp-ui company-lsp zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens rainbow-mode rainbow-delimiters projectile operate-on-number nlinum move-text markdown-mode magit lv json-mode js2-mode imenu-anywhere hl-todo guru-mode gruber-darker-theme gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell evil-visualstar evil-surround evil-numbers elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl crux counsel company browse-kill-ring anzu ag ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
