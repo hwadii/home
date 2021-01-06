d3e39a7bb762ef93715e448514009183b6d4487e
Wadii Hajji <wadii@cardiologs.com>
2021-01-07 00:28:29 +0100

    misc

--- config/emacs/custom.el
+++ config/emacs/custom.el
@@ -52,7 +52,7 @@
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (ng2-mode emmet-mode modus-vivendi-theme modus-operandi-theme csv-mode ssh-agency seoul256-theme yaml-mode web-mode tide yari inf-ruby company-anaconda anaconda-mode lsp-ui company-lsp zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens rainbow-mode rainbow-delimiters projectile operate-on-number nlinum move-text markdown-mode magit lv json-mode js2-mode imenu-anywhere hl-todo guru-mode gruber-darker-theme gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell evil-visualstar evil-surround evil-numbers elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl crux counsel company browse-kill-ring anzu ag ace-window)))
+    (php-mode go-mode ng2-mode emmet-mode modus-vivendi-theme modus-operandi-theme csv-mode ssh-agency seoul256-theme yaml-mode web-mode tide yari inf-ruby company-anaconda anaconda-mode lsp-ui company-lsp zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens rainbow-mode rainbow-delimiters projectile operate-on-number nlinum move-text markdown-mode magit lv json-mode js2-mode imenu-anywhere hl-todo guru-mode gruber-darker-theme gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell evil-visualstar evil-surround evil-numbers elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl crux counsel company browse-kill-ring anzu ag ace-window)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(vc-annotate-background nil)
  '(vc-annotate-background-mode nil)
