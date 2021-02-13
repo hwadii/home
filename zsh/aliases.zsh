3336c635087441371ec2a27f2fb45fe68df22783
Wadii Hajji <wadii@cardiologs.com>
2021-02-13 18:38:41 +0100

    new things

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -64,6 +64,30 @@ function blob() {
   echo "$@" | sed "s|blob://|s3://|g" | xargs s3cmd -c ~/.s3cfg-blob | sed "s|s3://|blob://|g" | sed "s|s3cmd |blob |g";
 }
 
+function up() {
+  curl -F"file=@$@" http://0x0.st
+}
+
+function open_command() {
+  local open_cmd
+
+  # define the open command
+  case "$OSTYPE" in
+    darwin*)  open_cmd='open' ;;
+    cygwin*)  open_cmd='cygstart' ;;
+    linux*)   [[ "$(uname -r)" != *icrosoft* ]] && open_cmd='nohup xdg-open' || {
+                open_cmd='cmd.exe /c start ""'
+                [[ -e "$1" ]] && { 1="$(wslpath -w "${1:a}")" || return 1 }
+              } ;;
+    msys*)    open_cmd='start ""' ;;
+    *)        echo "Platform $OSTYPE not supported"
+              return 1
+              ;;
+  esac
+
+  ${=open_cmd} "$@" &>/dev/null
+}
+
 alias ytdl=youtube-dl
 
 # more git aliases
