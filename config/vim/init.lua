4481e022de5741aa784a8055c4e895227d103cdc
Wadii Hajji <wadii@cardiologs.com>
2021-04-05 15:52:47 +0200

    github

--- config/vim/init.lua
+++ config/vim/init.lua
@@ -19,11 +19,11 @@ require('plugins')
 require 'colorizer'.setup ({}, { names = false })
 require('gitsigns').setup {
   signs = {
-    add = {hl = 'DiffAdd'   , text = '│'},
-    change = {hl = 'DiffChange', text = '│'},
-    delete = {hl = 'DiffDelete', text = '_'},
-    topdelete = {hl = 'DiffDelete', text = '‾'},
-    changedelete = {hl = 'DiffChange', text = '~'},
+    add          = {hl = 'DiffAdd'   , text = '│', numhl='GitSignsAddNr'},
+    change       = {hl = 'DiffChange', text = '│', numhl='GitSignsChangeNr'},
+    delete       = {hl = 'DiffDelete', text = '_', numhl='GitSignsDeleteNr'},
+    topdelete    = {hl = 'DiffDelete', text = '‾', numhl='GitSignsDeleteNr'},
+    changedelete = {hl = 'DiffChange', text = '~', numhl='GitSignsChangeNr'},
   }
 }
 require('neogit').setup({})
--- config/vim/plugin/colors.vim
+++ config/vim/plugin/colors.vim
@@ -1,4 +1,4 @@
-hi StatusLine cterm=reverse ctermfg=95 ctermbg=187 gui=reverse guibg=#DFDEBD guifg=#2A2622
+hi StatusLine cterm=reverse ctermfg=95 ctermbg=187 gui=reverse guibg=#DFDEBD guifg=#0d1117
 hi StatusLineNC cterm=italic,reverse ctermfg=235 ctermbg=187 gui=italic,reverse guifg=#333233 guibg=#9e9e9e
 hi User1 gui=bold guifg=#252525 guibg=#B6D6FD
 hi User2 gui=bold guifg=#af87d7
