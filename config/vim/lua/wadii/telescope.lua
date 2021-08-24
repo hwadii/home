03773898d50f7cd4fc3655cad4ecdc1d3d682e65
Wadii Hajji <wadii@cardiologs.com>
2021-08-24 01:15:14 +0100

    nvim/telescope: smart_case

--- config/vim/lua/wadii/telescope.lua
+++ config/vim/lua/wadii/telescope.lua
@@ -45,8 +45,9 @@ require('telescope').setup{
   },
   extensions = {
     fzy_native = {
-      override_generic_sorter = true,
-      override_file_sorter = true,
+      override_generic_sorter = false, -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
     },
   },
 }
