e3583054e2edc6f67fbcf687f0f330c5e9d9f206
Wadii Hajji <wadii@cardiologs.com>
2021-09-21 14:14:42 +0200

    nvim/plugins: move packer_compiled to lua folder

--- config/vim/lua/packer_compiled.lua
+++ config/vim/lua/packer_compiled.lua
@@ -215,14 +215,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: nvim-colorizer.lua
-time([[Config for nvim-colorizer.lua]], true)
-try_loadstring("\27LJ\2\nI\0\0\4\0\4\0\b6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0005\3\3\0B\0\3\1K\0\1\0\1\0\1\nnames\1\nsetup\14colorizer\frequire\0", "config", "nvim-colorizer.lua")
-time([[Config for nvim-colorizer.lua]], false)
 -- Config for: gitsigns.nvim
 time([[Config for gitsigns.nvim]], true)
 try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0", "config", "gitsigns.nvim")
 time([[Config for gitsigns.nvim]], false)
+-- Config for: nvim-colorizer.lua
+time([[Config for nvim-colorizer.lua]], true)
+try_loadstring("\27LJ\2\nI\0\0\4\0\4\0\b6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0005\3\3\0B\0\3\1K\0\1\0\1\0\1\nnames\1\nsetup\14colorizer\frequire\0", "config", "nvim-colorizer.lua")
+time([[Config for nvim-colorizer.lua]], false)
 if should_profile then save_profiles() end
 
 end)
--- config/vim/lua/plugins.lua
+++ config/vim/lua/plugins.lua
@@ -1,6 +1,6 @@
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function()
+return require('packer').startup({function()
   use {'wbthomason/packer.nvim', opt = true}
 
   use 'AndrewRadev/splitjoin.vim'
@@ -50,11 +50,9 @@ return require('packer').startup(function()
   use 'tpope/vim-abolish'
   use 'tpope/vim-commentary'
   use 'tpope/vim-surround'
-  use {
-    'tpope/vim-unimpaired',
-    commit = 'c51ed445182334aabd392f295708ebe8677bd8e8'
-  }
+  use 'tpope/vim-unimpaired'
   use 'tpope/vim-sleuth'
   use 'tpope/vim-vinegar'
   use 'lewis6991/impatient.nvim'
-end)
+end, config = { compile_path = vim.fn.stdpath('config') .. '/lua/packer_compiled.lua' }
+})
