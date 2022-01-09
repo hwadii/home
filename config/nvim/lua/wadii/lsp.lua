09442f8442dbce6f46281622fcb84899843cd78a
Wadii Hajji <wadii@cardiologs.com>
2022-01-09 18:03:36 +0100

    nvim/lsp: fix eslint builtin name

--- config/nvim/lua/wadii/lsp.lua
+++ config/nvim/lua/wadii/lsp.lua
@@ -4,7 +4,8 @@ local null_ls = require('null-ls')
 null_ls.setup({
   sources = {
     null_ls.builtins.code_actions.gitsigns,
-    null_ls.builtins.code_actions.eslint,
+    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.formatting.eslint_d,
   }
 })
 
