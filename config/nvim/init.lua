88eff84b2d885fdb27d79e9824e3b0c087dad122
Wadii Hajji <wadii@cardiologs.com>
2022-04-05 22:25:41 +0200

    nvim: switch to tokyobones

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,14 +5,14 @@ require('impatient')
 require('wadii')
 require('plugin.treesitter')
 
-vim.g.zenbones = {
+vim.g.tokyobones = {
   lighten_noncurrent_window = true,
   solid_float_border = true,
   solid_vert_split = true,
   solid_line_nr = true,
   darken_non_text = 30,
 }
-vim.cmd 'colorscheme zenbones'
+vim.cmd 'colorscheme tokyobones'
 opt.encoding = 'utf-8'
 opt.magic = true
 opt.autoindent = true
