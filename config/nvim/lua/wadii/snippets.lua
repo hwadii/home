5e10c7bd6a0c3399750a46f66a18fc14237b99f4
Wadii Hajji <wadii@cardiologs.com>
2022-04-06 23:07:01 +0200

    nvim/snippts: use proper methods to create snippets

--- config/nvim/lua/wadii/snippets.lua
+++ config/nvim/lua/wadii/snippets.lua
@@ -1,31 +1,29 @@
 local ls = require('luasnip')
 local s = ls.snippet
-local sn = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
 
 vim.api.nvim_set_keymap('s', '<a-j>', '<Plug>luasnip-next-choice', {})
 vim.api.nvim_set_keymap('i', '<a-j>', '<Plug>luasnip-next-choice', {})
 
-ls.snippets = {
-  all = {
-    s('todo', {
-      t('TODO(wadii): '),
-      i(0),
-    }),
-    s('xxx', {
-      t('XXX(wadii): '),
-      i(0),
-    })
-  },
-  ruby = {
-    s('rld', {
-      t('Rails.logger.debug'),
-      i(0),
-    }),
-    s('rli', {
-      t('Rails.logger.info'),
-      i(0),
-    })
-  }
-}
+ls.add_snippets('all', {
+  s('todo', {
+    t('TODO(wadii): '),
+    i(0),
+  }),
+  s('xxx', {
+    t('XXX(wadii): '),
+    i(0),
+  }),
+})
+
+ls.add_snippets('ruby', {
+  s('rld', {
+    t('Rails.logger.debug'),
+    i(0),
+  }),
+  s('rli', {
+    t('Rails.logger.info'),
+    i(0),
+  }),
+})
