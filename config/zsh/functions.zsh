c6eb9b09aa7597efc25120a4c3f55202f81a77c8
Wadii Hajji <wadii@cardiologs.com>
2022-04-12 23:52:10 +0200

    zsh/functions: rename fe into fp. Add fe without preview

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -48,6 +48,11 @@ function take {
 #   - Bypass fuzzy finder if there's only one match (--select-1)
 #   - Exit if there's no match (--exit-0)
 fe() (
+  IFS=$'\n' files=($(fzf --query="$1" --multi --select-1 --exit-0))
+  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+)
+
+fp() (
   IFS=$'\n' files=($(fzf --query="$1" --multi --select-1 --exit-0 --preview-window '~1' --preview "bat --style header,changes --color=always --decorations=always {} | head -500"))
   [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
 )
