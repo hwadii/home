ad6341f401f6af59cf772b7c9a07d37e05517825
Wadii Hajji <wadii@cardiologs.com>
2022-04-28 11:36:20 +0200

    nvim/telescope: Add shortcut to delete buffer forcefully

--- config/nvim/lua/wadii/telescope.lua
+++ config/nvim/lua/wadii/telescope.lua
@@ -1,5 +1,6 @@
 local telescope = require('telescope')
 local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
 telescope.load_extension('fzf')
 telescope.load_extension('neoclip')
 telescope.setup{
@@ -7,23 +8,23 @@ telescope.setup{
     path_display = { 'shorten' },
     mappings = {
       i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<C-q>"] = actions.send_to_qflist,
-        ["<a-q>"] = actions.send_selected_to_qflist,
-        ["<esc>"] = actions.close,
-        ["<C-[>"] = actions.close,
-        ["<C-c>"] = actions.close,
+        ['<C-j>'] = actions.move_selection_next,
+        ['<C-k>'] = actions.move_selection_previous,
+        ['<C-q>'] = actions.send_to_qflist,
+        ['<a-q>'] = actions.send_selected_to_qflist,
+        ['<esc>'] = actions.close,
+        ['<C-[>'] = actions.close,
+        ['<C-c>'] = actions.close,
       },
       n = {
-        ["<esc>"] = actions.close
+        ['<esc>'] = actions.close
       },
     },
   },
   pickers = {
     find_files = {
       theme = 'ivy',
-      find_command = { "fd", "--hidden", "-E.git", "-tf" },
+      find_command = { 'fd', '--hidden', '-E.git', '-tf' },
       preview = true,
       path_display = { 'smart' },
       layout_config = {
@@ -42,7 +43,13 @@ telescope.setup{
       },
       mappings = {
         i = {
-          ["<c-d>"] = actions.delete_buffer,
+          ['<c-d>'] = actions.delete_buffer,
+          ['<a-d>'] = function(prompt_bufnr)
+            local current_picker = action_state.get_current_picker(prompt_bufnr)
+            current_picker:delete_selection(function(selection)
+              vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+            end)
+          end,
         }
       }
     },
@@ -51,6 +58,6 @@ telescope.setup{
     fuzzy = true,                    -- false will only do exact matching
     override_generic_sorter = true,  -- override the generic sorter
     override_file_sorter = true,     -- override the file sorter
-    case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+    case_mode = 'smart_case',        -- or 'ignore_case' or 'respect_case'
   },
 }
