93d3c260cfc74d8113471fce75e807fbb8b3b3cd
Wadii Hajji <wadii@cardiologs.com>
2022-05-01 01:36:11 +0200

    nvim/lsp: Use server_capabilities

--- config/nvim/lua/wadii/lsp.lua
+++ config/nvim/lua/wadii/lsp.lua
@@ -60,8 +60,8 @@ lspconfig.tsserver.setup({
   },
   on_attach = function(client, bufnr)
     custom_attach(client, bufnr)
-    -- client.resolved_capabilities.document_formatting = false
-    -- client.resolved_capabilities.document_range_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
     local ts_utils = require('nvim-lsp-ts-utils')
     ts_utils.setup({
       eslint_bin = "eslint_d",
