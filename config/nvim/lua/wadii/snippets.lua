2a53cf0a43f3b565310c171f15af2834f5a93a77
Wadii Hajji <wadii@cardiologs.com>
2022-05-02 11:46:27 +0200

    nvim/snippets: Add date snippet

--- config/nvim/lua/wadii/snippets.lua
+++ config/nvim/lua/wadii/snippets.lua
@@ -7,23 +7,22 @@ vim.api.nvim_set_keymap('s', '<a-j>', '<Plug>luasnip-next-choice', {})
 vim.api.nvim_set_keymap('i', '<a-j>', '<Plug>luasnip-next-choice', {})
 
 ls.add_snippets('all', {
+  s('date', {
+    t(os.date('%Y-%m-%d')),
+  }),
   s('todo', {
     t('TODO(wadii): '),
-    i(0),
   }),
   s('xxx', {
     t('XXX(wadii): '),
-    i(0),
   }),
 })
 
 ls.add_snippets('ruby', {
   s('rld', {
-    t('Rails.logger.debug'),
-    i(0),
+    t('Rails.logger.debug '),
   }),
   s('rli', {
-    t('Rails.logger.info'),
-    i(0),
+    t('Rails.logger.info '),
   }),
 })
