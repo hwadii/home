20c9e3eb434a88a8abe5d97418cc3d2100303c50
Wadii Hajji <wadii@cardiologs.com>
2022-05-06 23:08:05 +0200

    nvim/telescope: Better telescope maps

--- config/nvim/lua/wadii/maps.lua
+++ config/nvim/lua/wadii/maps.lua
@@ -1,44 +1,44 @@
+local telescope = require('telescope.builtin')
+local map = vim.keymap.set
+
 vim.g.mapleader = ','
 vim.g.maplocalleader = ' '
 
 -- general
-vim.keymap.set("n", "<A-j>", "<cmd>m .+1<CR>==")
-vim.keymap.set("n", "<A-k>", "<cmd>m .-2<CR>==")
-vim.keymap.set("x", "<A-j>", "<cmd>m '>+1<CR>gv=gv")
-vim.keymap.set("x", "<A-k>", "<cmd>m '<-2<CR>gv=gv")
-vim.keymap.set("n", "<esc>", "<cmd>noh<return><esc>")
-vim.keymap.set("n", "<C-/>", "<cmd>set hlsearch!<cr>")
-vim.keymap.set("n", "<A-[>", "<cmd>vertical resize -5<cr>")
-vim.keymap.set("n", "<A-]>", "<cmd>vertical resize +5<cr>")
-vim.keymap.set("n", "[t", "gT")
-vim.keymap.set("n", "]t", "gt")
-vim.keymap.set("t", "<M-[>", "<C-\\><C-n>")
+map('n', '<A-j>', '<cmd>m .+1<CR>==')
+map('n', '<A-k>', '<cmd>m .-2<CR>==')
+map("x", "<A-j>", "<cmd>m '>+1<CR>gv=gv")
+map("x", "<A-k>", "<cmd>m '<-2<CR>gv=gv")
+map('n', '<esc>', '<cmd>noh<return><esc>')
+map('n', '<C-/>', '<cmd>set hlsearch!<cr>')
+map('n', '<A-[>', '<cmd>vertical resize -5<cr>')
+map('n', '<A-]>', '<cmd>vertical resize +5<cr>')
+map('n', '[t', 'gT')
+map('n', ']t', 'gt')
+map('t', '<M-[>', '<C-\\><C-n>')
 
 -- telescope
-vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>")
-vim.keymap.set("n", "<Leader>tt", "<cmd>Telescope<cr>")
-vim.keymap.set("n", "<Leader>s", "<cmd>Telescope live_grep<cr>")
-vim.keymap.set("n", "<Leader>tg", "<cmd>Telescope grep_string<cr>")
-vim.keymap.set("n", "<localleader>b", "<cmd>Telescope buffers<cr>")
-vim.keymap.set("n", "<Leader>th", "<cmd>Telescope oldfiles<cr>")
-vim.keymap.set("n", "<Leader>tc", "<cmd>Telescope git_commits<cr>")
-vim.keymap.set("n", "<Leader>to", "<cmd>Telescope lsp_document_symbols<cr>")
-vim.keymap.set("n", "<Leader>tr", "<cmd>Telescope lsp_references<cr>")
-vim.keymap.set("n", "<Leader>te", "<cmd>Telescope resume<cr>")
-vim.keymap.set("n", "<Leader>t;", "<cmd>Telescope neoclip<cr>")
-vim.keymap.set("n", "<C-s>", "<cmd>Telescope current_buffer_fuzzy_find<cr>")
+map('n', '<C-p>', telescope.find_files)
+map('n', '<Leader>?', telescope.builtin)
+map('n', '<Leader>/', telescope.live_grep)
+map('n', '<Leader>sw', telescope.grep_string)
+map('n', '<localleader><localleader>', telescope.buffers)
+map('n', '<Leader>s?', telescope.oldfiles)
+map('n', '<Leader>sc', telescope.git_commits)
+map('n', '<Leader>se', telescope.resume)
+map('n', '<C-s>', telescope.current_buffer_fuzzy_find)
 
 -- git
-vim.keymap.set("n", "<Leader>gs", "<cmd>Git<cr>")
-vim.keymap.set("n", "<Leader>gc", "<cmd>Git commit<cr>")
-vim.keymap.set("n", "<Leader>gp", "<cmd>Git push<cr>")
-vim.keymap.set("n", "<Leader>gl", "<cmd>Git log<cr>")
-vim.keymap.set("n", "<Leader>gd", "<cmd>Git diff<cr>")
-vim.keymap.set("n", "<Leader>gb", "<cmd>Git blame<cr>")
-vim.keymap.set("n", "<Leader>ga", "<cmd>Git branch<cr>")
+map('n', '<Leader>gs', '<cmd>Git<cr>')
+map('n', '<Leader>gc', '<cmd>Git commit<cr>')
+map('n', '<Leader>gp', '<cmd>Git push<cr>')
+map('n', '<Leader>gl', '<cmd>Git log<cr>')
+map('n', '<Leader>gd', '<cmd>Git diff<cr>')
+map('n', '<Leader>gb', '<cmd>Git blame<cr>')
+map('n', '<Leader>ga', '<cmd>Git branch<cr>')
 
 -- colorizer
-vim.keymap.set("n", "<Leader>c", "<cmd>ColorizerToggle<cr>")
+map('n', '<Leader>c', '<cmd>ColorizerToggle<cr>')
 
 -- custom commands
 
