717b9ce90b4877b3c25708a9d6b65cb9cbda332b
Wadii Hajji <wadii@cardiologs.com>
2022-07-08 21:13:52 +0200

    nvim/treesitter: Add sql in ensure_installed

--- config/nvim/lua/plugin/treesitter.lua
+++ config/nvim/lua/plugin/treesitter.lua
@@ -1,7 +1,8 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {
     "ruby", "typescript", "scheme", "go", "javascript", "tsx", "vim", "lua",
-    "rust", "css", "dot", "json", "python", "yaml", "vue", "markdown", "html"
+    "rust", "css", "dot", "json", "python", "yaml", "vue", "markdown", "html",
+    "sql"
   },
   textobjects = {
     select = {
