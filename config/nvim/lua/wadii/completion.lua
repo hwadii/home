e7eddd4902f9f13c6993900c06b25845f8c993ae
Wadii Hajji <wadii@cardiologs.com>
2022-08-23 17:02:56 +0100

    nvim/completion: Disable github completion

--- config/nvim/lua/wadii/completion.lua
+++ config/nvim/lua/wadii/completion.lua
@@ -37,7 +37,6 @@ cmp.setup {
   sources = cmp.config.sources({
       { name = "nvim_lsp" },
       { name = "luasnip" },
-      { name = "git" }
     }, {
       { name = "buffer", option = { keyword_length = 5, max_item_count = 5 } },
       { name = "path" },
