ed9466e649e93ca4782089a7b3d2bfc2955fdfa9
Wadii Hajji <wadii@cardiologs.com>
2023-02-03 18:51:10 +0100

    nvim/various: Reword notification

--- config/nvim/lua/wadii/various.lua
+++ config/nvim/lua/wadii/various.lua
@@ -7,7 +7,7 @@ M.yank_path = function(bufnr, register)
   local relative_path = Path:new(vim.api.nvim_buf_get_name(bufnr)):make_relative(vim.loop.cwd())
   vim.fn.setreg(register, relative_path)
   if #relative_path ~= 0 then
-    vim.notify('Path copied to register "' .. register)
+    vim.notify('Path yanked into "' .. register)
   end
 end
 
@@ -18,7 +18,7 @@ M.yank_current_path = function(register)
   local relative_path = Path:new(vim.fn.getreg("%")):make_relative(vim.loop.cwd())
   vim.fn.setreg(register, relative_path)
   if #relative_path ~= 0 then
-    vim.notify('Path copied to register "' .. register)
+    vim.notify('Path yanked into "' .. register)
   end
 end
 
