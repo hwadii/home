6bfd1eaa0dabae6cd66d15885c9c54e3ffff5847
Wadii Hajji <wadii@cardiologs.com>
2023-03-05 23:22:00 +0100

    fzf: Use fd

--- config/fish/functions/fe.fish
+++ config/fish/functions/fe.fish
@@ -1,4 +1,4 @@
 function fe -d "Open the selected file with the default editor"
-    set files (fzf --query="$argv[1]" --multi --select-1 --exit-0)
+    set files (fd -tf | fzf --query="$argv[1]" --multi --select-1 --exit-0)
     test -n "$files" && $EDITOR $files
 end
