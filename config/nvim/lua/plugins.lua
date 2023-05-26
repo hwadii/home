f585018c8f1935399c13a4d1c0aff3ef0210d1ce
Wadii Hajji <wadii@cardiologs.com>
2023-05-26 09:04:02 +0200

    nvim/telescope: Build fzf-native plugin in release mode

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -115,7 +115,7 @@ return {
       'nvim-lua/plenary.nvim',
     }
   },
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
   { 'nvim-telescope/telescope-ui-select.nvim' },
   'jose-elias-alvarez/null-ls.nvim',
   {
