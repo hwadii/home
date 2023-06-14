fee36049af73a1373d67eeb47aeb6d9ba521a585
Wadii Hajji <wadii@cardiologs.com>
2023-06-14 15:50:29 +0200

    nvim/gitsigns: Set different keybinds for visual stage and reset

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -92,8 +92,10 @@ return {
           end, { expr = true })
 
           -- Actions
-          map({ 'n', 'v' }, '<leader>hs', gs.stage_hunk)
-          map({ 'n', 'v' }, '<leader>hr', gs.reset_hunk)
+          map('n', '<leader>hs', gs.stage_hunk)
+          map('n', '<leader>hr', gs.reset_hunk)
+          map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+          map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
           map('n', '<leader>hS', gs.stage_buffer)
           map('n', '<leader>hu', gs.undo_stage_hunk)
           map('n', '<leader>hR', gs.reset_buffer)
