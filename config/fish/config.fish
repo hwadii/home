ea4f50a4cb8412f48bef3c7d434481af36351393
Wadii Hajji <wadii@cardiologs.com>
2023-07-31 10:03:46 +0200

    fish: Remove ssh-agent code

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,10 +2,3 @@ set fish_greeting
 
 zoxide init fish | source
 rtx activate fish | source
-
-if status --is-interactive; and not set -q SSH_AUTH_SOCK
-    eval (ssh-agent -c)
-    function kill_ssh_agent_on_exit --on-event fish_exit
-        ssh-agent -k >/dev/null
-    end
-end
