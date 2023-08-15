15db89db60fa9565bb647053ea94b0ea8a8b0244
Wadii Hajji <wadii@cardiologs.com>
2023-08-15 11:41:06 +0200

    fish: Add aliases for tsh + kubectl

---
+++ config/fish/conf.d/kubectl.fish
@@ -0,0 +1,13 @@
+alias k "tsh kubectl"
+alias kls "tsh kube ls"
+alias klogin "tsh kube login"
+
+alias kgp "tsh kubectl get pods"
+alias kgpwide "kgp -o wide"
+alias kgpw "kgp --watch"
+
+alias kgs "tsh kubectl get svc"
+alias kgswide "kgs -o wide"
+alias kgsw "kgs --watch"
+
+alias keti "tsh kubectl exec -it"
