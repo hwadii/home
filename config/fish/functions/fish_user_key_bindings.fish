c77f86eb704c52cbfe1f57f240c7387110d9e137
Wadii Hajji <wadii@cardiologs.com>
2023-11-20 11:18:44 +0100

    fish: Erase fzf default bindings

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 function fish_user_key_bindings
     fzf_key_bindings
+    bind --erase \ct
+    bind --erase \cr
     bind --preset \el downcase-word
     bind \e\; fzf-file-widget
-    bind \ec fzf-cd-widget
 end
