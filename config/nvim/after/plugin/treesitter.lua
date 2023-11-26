9250fed2ea282c0754acc632fc2c1968e89626e7
Wadii Hajji <wadii@cardiologs.com>
2023-11-26 20:02:01 +0100

    nvim/treesitter: Add language

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -4,7 +4,7 @@ require 'nvim-treesitter.configs'.setup {
     "rust", "css", "dot", "json", "python", "yaml", "vue", "org", "markdown",
     "markdown_inline", "html", "sql", "c_sharp", "fish", "diff", "git_rebase",
     "gitignore", "gitattributes", "vimdoc", "nix", "comment", "hare", "hcl", "groovy",
-    "csv", "racket", "terraform"
+    "csv", "racket", "terraform", "angular"
   },
   textobjects = {
     select = {
