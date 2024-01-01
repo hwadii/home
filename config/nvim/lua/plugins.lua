b04cb7ceafb73a9898e54842c7f70303df691b6c
Wadii Hajji <wadii@cardiologs.com>
2024-01-01 23:54:32 +0100

    nvim: Fix comment.nvim lazy load condition

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -77,7 +77,7 @@ return {
         pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
       })
     end,
-    keys = { "gc" },
+    event = "BufReadPre",
     dependencies = 'JoosepAlviste/nvim-ts-context-commentstring',
   },
   {
