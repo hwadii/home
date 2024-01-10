17b9796467f023fe60335962b9cdba717d1cb31b
Wadii Hajji <wadii@cardiologs.com>
2024-01-10 11:24:02 +0100

    nvim/plugins: remove useless cmp binding

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -116,9 +116,8 @@ return {
       opts.mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
       })
       opts.window = {
         documentation = cmp.config.window.bordered({
