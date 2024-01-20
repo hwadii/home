48b0063146a005a4996570aa71c3474c986c58a5
Wadii Hajji <wadii@cardiologs.com>
2024-01-20 19:14:43 +0100

    fish: rework environment variables

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,22 @@
 set fish_greeting
 
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.local/bin
+
+set -x LESS " --mouse -Ri"
+set -x PAGER less
+set -x MANWIDTH 80
+
+set -x XDG_CONFIG_HOME ~/.config
+set -x RIPGREP_CONFIG_PATH ~/.config/ripgrep/ripgreprc
+set -x PASSWORD_STORE_DIR ~/.local/share/pass/
+set -x LANG en_US
+set -x LANGUAGE en_US
+set -x JQ_COLORS "2;37:0;37:0;37:0;37:0;32:1;37:1;37"
+set -x CARGO_TARGET_DIR ~/.local/cargo/target
+
 zoxide init fish | source
+mise activate fish | source
