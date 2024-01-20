64a45129161e6300b8c417f098ee246d1ea26a89
Wadii Hajji <wadii@cardiologs.com>
2024-01-20 19:21:57 +0100

    fish: fix locales

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,8 +13,9 @@ set -x MANWIDTH 80
 set -x XDG_CONFIG_HOME ~/.config
 set -x RIPGREP_CONFIG_PATH ~/.config/ripgrep/ripgreprc
 set -x PASSWORD_STORE_DIR ~/.local/share/pass/
-set -x LANG en_US
-set -x LANGUAGE en_US
+set -x LANG en_US.utf8
+set -x LANGUAGE en_US.utf8
+set -x LC_ALL fr_FR
 set -x JQ_COLORS "2;37:0;37:0;37:0;37:0;32:1;37:1;37"
 set -x CARGO_TARGET_DIR ~/.local/cargo/target
 
