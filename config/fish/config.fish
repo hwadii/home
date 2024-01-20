031e13912a8e9b81b9e5829dec031d2cfc69d75d
Wadii Hajji <wadii@cardiologs.com>
2024-01-20 23:39:40 +0100

    fish: fix LC_ALL

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -15,7 +15,7 @@ set -x RIPGREP_CONFIG_PATH ~/.config/ripgrep/ripgreprc
 set -x PASSWORD_STORE_DIR ~/.local/share/pass/
 set -x LANG en_US.utf8
 set -x LANGUAGE en_US.utf8
-set -x LC_ALL fr_FR
+set -x LC_ALL fr_FR.UTF-8
 set -x JQ_COLORS "2;37:0;37:0;37:0;37:0;32:1;37:1;37"
 set -x CARGO_TARGET_DIR ~/.local/cargo/target
 
