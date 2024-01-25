9bd3a96f7ded330b182584adaff4c1ab42a9266d
Wadii Hajji <wadii@cardiologs.com>
2024-01-25 09:29:56 +0100

    fish: fix locales

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -16,9 +16,9 @@ set -x MANWIDTH 80
 set -x XDG_CONFIG_HOME ~/.config
 set -x RIPGREP_CONFIG_PATH ~/.config/ripgrep/ripgreprc
 set -x PASSWORD_STORE_DIR ~/.local/share/pass/
-set -x LANG en_US.utf8
-set -x LANGUAGE en_US.utf8
-set -x LC_ALL fr_FR.UTF-8
+set -x LANG en_US.UTF-8
+set -x LANGUAGE en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
 set -x JQ_COLORS "2;37:0;37:0;37:0;37:0;32:1;37:1;37"
 
 set -x DOTNET_ROOT (mise where dotnet-core)
