6bd7162f150924a186d59c67cce9c470f89ce419
Wadii Hajji <wadii@cardiologs.com>
2024-01-25 09:28:35 +0100

    fish: use eval

--- config/fish/functions/vg.fish
+++ config/fish/functions/vg.fish
@@ -1,6 +1,6 @@
 function vg -d "Grep pattern and select file where it appears"
-    set ret (rg --line-number --no-heading $argv | fzf -0 -1 | awk -F: '{print $1, $2}' | string split " ")
+    set ret (rg --line-number --no-heading $argv | fzf -0 -1 --multi | awk -F: '{print $1, $2}' | string split " ")
     set file $ret[1]
     set line $ret[2]
-    test -n "$file" && nvim $file +$line
+    test -n "$file" && eval "$EDITOR $file +$line"
 end
