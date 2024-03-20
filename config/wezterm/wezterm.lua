85d8d4ab6d0bb79a150243bea77e80c0584ba15c
Wadii Hajji <hajji.wadii@yahoo.fr>
2024-03-20 10:28:38 +0100

    wezterm: disable tab bar on macos

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,11 +2,12 @@ local wezterm = require 'wezterm'
 local mux = wezterm.mux
 local config = wezterm.config_builder()
 
+config.default_prog = { '/opt/homebrew/bin/fish', '-c', 'tmux new-session -ADs x' }
 config.term = 'wezterm'
 config.font = wezterm.font_with_fallback { 'Berkeley Mono', 'Symbols Nerd Font Mono' }
-config.font_size = 15
+config.font_size = 14.5
 config.underline_thickness = 1
-config.enable_tab_bar = true
+config.enable_tab_bar = false
 config.tab_bar_at_bottom = true
 config.window_close_confirmation = "NeverPrompt"
 config.use_ime = false
