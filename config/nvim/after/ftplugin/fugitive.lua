80351e9aefb8afccece740782ccb54e2f29f1c9b
Wadii Hajji <hajji.wadii@yahoo.fr>
2024-03-29 19:19:42 +0100

    fugitive: pf

--- config/nvim/after/ftplugin/fugitive.lua
+++ config/nvim/after/ftplugin/fugitive.lua
@@ -1,4 +1,7 @@
 vim.keymap.set("n", "pp", "<cmd>Git push<cr>", { buffer = true })
+vim.keymap.set("n", "pf", "<cmd>Git push --force-with-lease<cr>", { buffer = true })
 vim.keymap.set("n", "p<space>", ":<C-U>Git push<space>", { buffer = true })
 
 vim.keymap.set("n", "L", "<cmd>Git log<cr>", { buffer = true })
+
+vim.keymap.set("n", "<Tab>", "=", { buffer = true, remap = true })
