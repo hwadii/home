047dab36ccb125bd1db919c7bbe097d2e0e6e627
Wadii Hajji <hajji.wadii@yahoo.fr>
2024-04-09 10:47:47 +0200

    fish: add unrestricted option to file-edit

--- config/fish/functions/file-edit.fish
+++ config/fish/functions/file-edit.fish
@@ -1,5 +1,11 @@
 function file-edit -d "Open the selected file with the default editor"
-    set files (fd -tf --hidden | fzf --query="$argv[1]" --multi --select-1 --exit-0)
-    test -n "$files" && eval "$EDITOR $files"
+    argparse u/unrestricted -- $argv
+    if test -z $_flag_u
+        set files (fd -tf --hidden)
+    else
+        set files (fd -tf -u)
+    end
+    set -l selection (string split " " $files | fzf --query="$argv[1]" --multi --select-1 --exit-0)
+    test -n "$selection" && eval "$EDITOR $selection"
     commandline -f repaint
 end
