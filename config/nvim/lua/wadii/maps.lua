c38ceac8661a1d6709016babcb7e1630832a3e68
Wadii Hajji <hajji.wadii@yahoo.fr>
2024-04-24 19:37:50 +0200

    nvim: make <c-i> map work independently from tab

--- config/nvim/lua/wadii/maps.lua
+++ config/nvim/lua/wadii/maps.lua
@@ -1,22 +1,27 @@
-local map = vim.keymap.set
 local various = require("wadii.various")
 
 -- general
-map("n", "<A-[>", "<cmd>vertical resize -5<cr>", { desc = "Decrease window width" })
-map("n", "<A-]>", "<cmd>vertical resize +5<cr>", { desc = "Increase window width" })
-map("n", "<A-Up>", "<cmd>horizontal resize +5<cr>", { desc = "Increase window height" })
-map("n", "<A-Down>", "<cmd>horizontal resize -5<cr>", { desc = "Decrease window height" })
-map("n", "<Left>", "gT")
-map("n", "<Right>", "gt")
-map("t", "<M-[>", "<C-\\><C-n>")
-map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+vim.keymap.set("n", "<A-[>", "<cmd>vertical resize -5<cr>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<A-]>", "<cmd>vertical resize +5<cr>", { desc = "Increase window width" })
+vim.keymap.set("n", "<A-Up>", "<cmd>horizontal resize +5<cr>", { desc = "Increase window height" })
+vim.keymap.set(
+  "n",
+  "<A-Down>",
+  "<cmd>horizontal resize -5<cr>",
+  { desc = "Decrease window height" }
+)
+vim.keymap.set("n", "<Left>", "gT")
+vim.keymap.set("n", "<Right>", "gt")
+vim.keymap.set("t", "<M-[>", "<C-\\><C-n>")
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+vim.keymap.set("n", "<C-I>", "<C-I>", { noremap = true })
 
 -- various
-map(
+vim.keymap.set(
   "n",
   "<Leader>wp",
   various.yank_current_path,
   { desc = "Yank the current absolute path in clipboard register" }
 )
-map("n", "<Leader>wu", various.make_unix, { desc = "Make fileformat unix" })
-map("n", "<Leader>w-", "<cmd>Explore<cr>", { desc = "Open netrw" })
+vim.keymap.set("n", "<Leader>wu", various.make_unix, { desc = "Make fileformat unix" })
+vim.keymap.set("n", "<Leader>w-", "<cmd>Explore<cr>", { desc = "Open netrw" })
