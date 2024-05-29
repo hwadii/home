82d3241f96d7f3be26e7faf2273d590a1e9b6079
Wadii Hajji <hajji.wadii@yahoo.fr>
2024-05-29 09:11:19 +0200

    nvim: use v:register in yank path

--- config/nvim/lua/wadii/various.lua
+++ config/nvim/lua/wadii/various.lua
@@ -3,7 +3,9 @@ local Path = require("plenary.path")
 
 M.yank_path = function(bufnr, register)
   bufnr = bufnr or 0
-  register = register or "+"
+  register = register or vim.v.register or "\""
+  -- Make relative in case "%" does not contain the relative path for some
+  -- reason.
   local relative_path = Path:new(vim.api.nvim_buf_get_name(bufnr)):make_relative(vim.loop.cwd())
   vim.fn.setreg(register, relative_path)
   if #relative_path ~= 0 then
@@ -12,9 +14,6 @@ M.yank_path = function(bufnr, register)
 end
 
 M.yank_current_path = function(register)
-  register = register or "+"
-  -- Make relative in case "%" does not contain the relative path for some
-  -- reason.
   M.yank_path(0, register)
 end
 
