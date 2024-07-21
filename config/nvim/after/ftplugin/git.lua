5f89384e144dda005147ee12120ae6aebbcf114f
Wadii Hajji <hajji.wadii@yahoo.fr>
2024-07-21 14:11:56 +0100

    nvim: remove useless map

--- config/nvim/after/ftplugin/git.lua
+++ config/nvim/after/ftplugin/git.lua
@@ -1,4 +1,2 @@
 vim.opt_local.spell = false
-
-vim.keymap.set({ "n", "x" }, "<Enter>", function() require("mini.git").show_at_cursor() end, { buffer = true, desc = "Show at cursor" })
 vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = true })
