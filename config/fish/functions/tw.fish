0fc10735492b5e860d40c6c846d485054e3099a3
Wadii Hajji <hajji.wadii@yahoo.fr>
2024-07-21 14:11:33 +0100

    fish: update scripts

--- config/fish/functions/tw.fish
+++ config/fish/functions/tw.fish
@@ -1,3 +1,10 @@
 function tw
-    mpv https://twitch.tv/$argv --quiet
+    set streams "39daph" "EnglishBen" "English_Ben" "TheGreatReview" "ThePrimeagen" "dmmulroy" "louispilfold" "lpil" \
+         "papesan" "teej_dv" "theprimeagen" "tigerbeetle" "tsoding" "untangledco" "lcolonq"
+    set selection (string split " " $streams | fzf)
+    if test (uname) = "Darwin"
+        iina https://twitch.tv/$selection
+    else
+        mpv --quiet https://twitch.tv/$selection
+    end
 end
