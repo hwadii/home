7cc259ece2e21d615fc501b2877defad7d4fbc71
Wadii Hajji <hajji.wadii@yahoo.fr>
2024-07-31 13:41:09 +0100

    nvim/markdown: try stuff

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -3,3 +3,7 @@ vim.opt_local.spelllang = "en"
 vim.opt_local.textwidth = 80
 vim.opt_local.shiftwidth = 2
 vim.opt_local.conceallevel = 2
+vim.api.nvim_buf_create_user_command(0, "ToggleCheckbox", function()
+  vim.cmd("s/- \\[ \\]/- [x]/")
+  vim.cmd.nohl()
+end, {})
