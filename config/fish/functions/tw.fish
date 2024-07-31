faf86101564d41b307e0e87244edf642a94e33df
Wadii Hajji <hajji.wadii@yahoo.fr>
2024-07-31 13:41:24 +0100

    fish: tweak tw.fish

--- config/fish/functions/tw.fish
+++ config/fish/functions/tw.fish
@@ -1,7 +1,7 @@
 function tw
     set streams "39daph" "EnglishBen" "English_Ben" "TheGreatReview" "ThePrimeagen" "dmmulroy" "louispilfold" "lpil" \
-         "papesan" "teej_dv" "theprimeagen" "tigerbeetle" "tsoding" "untangledco" "lcolonq"
-    set selection (string split " " $streams | fzf)
+         "papesan" "teej_dv" "theprimeagen" "tigerbeetle" "tsoding" "untangledco" "lcolonq" "sphaerophoria" "etoiles"
+    set selection (string split " " $streams | sort | fzf)
     if test (uname) = "Darwin"
         iina https://twitch.tv/$selection
     else
