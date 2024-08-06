3b2269125afd625dd346d76d07e6107da4a0b6d6
Wadii Hajji <hajji.wadii@yahoo.fr>
2024-08-06 01:09:14 +0100

    nvim/treesitter: disable highlight on large files

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -120,6 +120,13 @@ return {
       },
       highlight = {
         enable = true,
+        disable = function(_, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
       },
       fold = {
         enable = true,
