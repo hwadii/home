3438fcb17bd6a69c83f3f11d8ca4f93e14cdcbab
Wadii Hajji <hajji.wadii@yahoo.fr>
2024-08-10 12:52:38 +0100

    nvim: powershell is in nvim-treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -177,15 +177,6 @@ return {
           detach = function() end,
         },
       })
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.powershell = {
-        install_info = {
-          url = "https://github.com/jrsconfitto/tree-sitter-powershell",
-          files = { "src/parser.c" },
-        },
-        filetype = "ps1",
-        used_by = { "psm1", "psd1", "pssc", "psxml", "cdxml" },
-      }
     end,
   },
 }
